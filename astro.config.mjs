a6695b9effdd13283db33ccbe804e38ce9a7ec01
abdulrm <abdo.mjahed@seresco.es>
2026-06-01 15:44:07 +0200

    feat: add GitHub Pages deployment workflow and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abdomjahed1.github.io',
+  base: 'sonvice-web',
   vite: {
     plugins: [tailwindcss()]
   }
